fd45b1a82b5c68ecc3fb5a56467215a2ea68987a
Sandra <lissetdovah@gmail.com>
2020-10-03 21:24:45 -0500

    Prueba js

---
+++ formulario.js
@@ -0,0 +1,27 @@
+var aciertos = 0, calif = 0, persona, resp;
+
+function show(){
+    /*resp = document.getElementById('resp');
+    resp.style.display = 'block';
+    alert("entra");*/
+}
+
+function calificar(){
+    persona = document.getElementById("nombre").value;
+
+    if(document.getElementById("comi1").checked && document.getElementById("comi2").checked)
+        aciertos++;
+    if(document.getElementById("numEst").value==32)
+        aciertos++;
+    if(document.getElementById("sig1").checked)
+        aciertos++;
+    if(document.getElementById("opc").value=="Todas las anteriores")
+        aciertos++;
+    if(document.getElementById("agui2").checked)
+        aciertos++;
+
+    calif = aciertos * 100 / 5;
+    show();
+
+    alert("Estimado "+persona+" tuvo "+aciertos+" de 5 aciertos\nCalificación: "+calif);
+}
