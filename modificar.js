103d0262ab409ab6fba4fdf2087dcfcfec4b9653
Sandra <lissetdovah@gmail.com>
2020-10-04 19:08:51 -0500

    Prueba Javascrpit AddEvent

--- modificar.js
+++ modificar.js
@@ -1,30 +1,40 @@
-console.log("Entra");
+//console.log("Entra");
 
-/*Aqui regresa null, dentro de una fuction no
-var primero = document.querySelector("#button_First");
-alert(primero);*/
-
-var primero = document.querySelector('#button_First'); //aqui regresa null, por eso el evento dice que se realiza
-primero.addEventListener("click",changeFisrt);
+window.onload = function(){
+    var titulo = document.querySelector('#button_Title');
+    titulo.addEventListener("click",changeTitle);
+    var primero = document.querySelector('#button_First');
+    primero.addEventListener("click",changeFisrt);
+    var segundo = document.querySelector('#button_Second');
+    segundo.addEventListener("click",changeSecond);
+    var estilo = document.querySelector('#button_Style');
+    estilo.addEventListener("click",changeAddStyle);
+}
 
 function changeTitle(){
-    var titulo = document.querySelector('#title');
+    titulo = document.querySelector('#conteiner__title');
     titulo.innerHTML = "Nuevo Titulo del Texto";
-
-    /*Aqui no regresa null, ------descomentar para probar y comprobar que aqui si entra---------
-    var primero = document.querySelector('#button_First');
-    primero.addEventListener("click",changeFisrt);*/
 }
 
 function changeFisrt(){
-    console.log("Entra al Event");
+    primero = document.querySelector('#conteiner__first');
+    primero.innerHTML = "1. Nuevo Texto del primer parrafo";
 }
 
 function changeSecond(){
-    
+    segundo = document.querySelector('#conteiner__second');
+    segundo.innerHTML = "2. Nuevo Texto del segundo parrafo";
 }
 
 function changeAddStyle(){
-    
+    estilo = document.getElementById('conteiner');
+    estilo.style.cssText = 'background-color: black; color: white; width: 80%; margin: 0 auto; font-size: 22px;';
+    estilo = document.getElementById('button_Title');
+    estilo.style.cssText = 'background-color: coral; color: white; font-size: 22px;';
+    estilo = document.getElementById('button_First');
+    estilo.style.cssText = 'background-color: pink; color: black; font-size: 22px;';
+    estilo = document.getElementById('button_Second');
+    estilo.style.cssText = 'background-color: white; color: black; font-size: 22px;';
+    estilo = document.getElementById('button_Style');
+    estilo.style.cssText = 'background-color: purple; color: white; font-size: 22px;';
 }
-
