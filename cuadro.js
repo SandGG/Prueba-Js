ea707b4848c83adb9471ba7d7c7f8a42d4de0373
Sandra <lissetdovah@gmail.com>
2020-10-26 17:40:09 -0600

    keyDown 1.1

--- cuadro.js
+++ cuadro.js
@@ -1,40 +1,43 @@
-document.addEventListener('keydown', logKey);
-
-function logKey(e) {
+document.addEventListener('keydown', (e) => {
     const elemento = document.querySelector('.container');
     const posicion = elemento.getBoundingClientRect();
     //console.log(posicion.top, posicion.right, posicion.bottom, posicion.left);
 
-    let posx = document.querySelector('.container__cuadro').offsetLeft; 
-    let posy = document.querySelector('.container__cuadro').offsetTop; 
-    let body = (document.querySelector('body').offsetWidth - 8) * 0.8 - 6; //Para quitar el margen, 80% corresponde al container y 14 de los bordes y margen
-    console.log(body);
+    let posx = document.querySelector('#cuadro').offsetLeft; 
+    let posy = document.querySelector('#cuadro').offsetTop; 
+    let body = document.querySelector('body').offsetWidth * 0.8 - 16; //Para quitar el margen, 80% corresponde al container y 16 de los bordes y margen
+    
+    console.log(posx, posy);
+
     if (e.key == 'ArrowUp') {
-        let arriba = document.querySelector('.container__cuadro').style.top = posy - 20; 
+        let arriba = document.querySelector('#cuadro').style.top = posy - 20; 
         if (arriba > 0) {
-            document.querySelector('.container__cuadro').style.top = arriba+'px';
+            document.querySelector('.container').innerHTML += '<div class="container__cuadro" style="left: '+posx+'px; top: '+posy+'px;"></div>';
+            document.querySelector('#cuadro').style.top = arriba+'px';
         }  
     } 
 
     if (e.key == 'ArrowRight') {
-        let derecha = document.querySelector('.container__cuadro').style.left = posx + 20;
-        console.log(derecha);
+        let derecha = document.querySelector('#cuadro').style.left = posx + 20;
         if (derecha < body) {
-            document.querySelector('.container__cuadro').style.left = derecha+'px'; 
+            document.querySelector('.container').innerHTML += '<div class="container__cuadro" style="left: '+posx+'px; top: '+posy+'px;"></div>';
+            document.querySelector('#cuadro').style.left = derecha+'px'; 
         }
     } 
 
     if (e.key == 'ArrowDown') {
-        let abajo = document.querySelector('.container__cuadro').style.top = posy + 20; 
-        if (abajo < (posicion.bottom - 14)) { //8 de margin en top y 6 por los bordes 
-            document.querySelector('.container__cuadro').style.top = abajo+'px';
+        let abajo = document.querySelector('#cuadro').style.top = posy + 20; 
+        if (abajo < (posicion.bottom - 16)) { //8 de margin en top y 8 por los bordes 
+            document.querySelector('.container').innerHTML += '<div class="container__cuadro" style="left: '+posx+'px; top: '+posy+'px;"></div>';
+            document.querySelector('#cuadro').style.top = abajo+'px';
         }
     } 
 
     if (e.key == 'ArrowLeft') {
-        let izq = document.querySelector('.container__cuadro').style.left = posx - 20; 
+        let izq = document.querySelector('#cuadro').style.left = posx - 20; 
         if (izq > 0) {
-            document.querySelector('.container__cuadro').style.left = izq+'px';
+            document.querySelector('.container').innerHTML += '<div class="container__cuadro" style="left: '+posx+'px; top: '+posy+'px;"></div>';
+            document.querySelector('#cuadro').style.left = izq+'px';
         }
     } 
-}
+});
